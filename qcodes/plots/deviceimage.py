efe4d1e1b3117ec640ba6da8c27329d64e971bf6
William Nielsen <whpn@mailbox.org>
2017-03-14 18:15:02 +0100

    feat: Add click-position functionality
    Add positions for labels and annotations by simple clicks.

--- qcodes/plots/deviceimage.py
+++ qcodes/plots/deviceimage.py
@@ -2,9 +2,11 @@
 
 import sys
 import os
+import time
 import PyQt5.QtWidgets as qt
 import PyQt5.QtGui as gui
 
+from functools import partial
 
 
 class MakeDeviceImage(qt.QWidget):
@@ -12,27 +14,40 @@ class MakeDeviceImage(qt.QWidget):
     Class for clicking and adding labels
     """
 
-    def __init__(self):
+    def __init__(self, app):
 
         super().__init__()
+
+        self.qapp = app
+
+        # BACKEND
+        self._data = {}
+
+        # FRONTEND
 
         grid = qt.QGridLayout()
         self.setLayout(grid)
 
         self.imageCanvas = qt.QLabel()
-        # gui.QPixmap
-        # self.imagelabel.setPixmap(...)
-        ###self.imageCanvas.setPixmap('/Users/william/Downloads/device800.jpeg')
-
         self.loadButton = qt.QPushButton('Load image')
         self.labelButton = qt.QPushButton('Insert Label')
         self.annotButton = qt.QPushButton('Place annotation')
+        self.channelLabel = qt.QLabel('Channel number')
+        self.channelNumber = qt.QLineEdit
 
         self.loadButton.clicked.connect(self.loadimage)
+        self.labelButton.clicked.connect(partial(self.setlabel_or_annotation,
+                                                 argument='label'))
+        self.annotButton.clicked.connect(partial(self.setlabel_or_annotation,
+                                                 argument='annotation'))
+        self.imageCanvas.mousePressEvent = None
+        self.imageCanvas.setStyleSheet('background-color: red')
 
-        grid.addWidget(self.imageCanvas, 0, 0, 4, 4)
+        grid.addWidget(self.imageCanvas, 0, 0, 4, 5)
         grid.addWidget(self.loadButton, 4, 0)
         grid.addWidget(self.labelButton, 4, 1)
+        grid.addWidget(self.annotButton, 4, 2)
+        grid.addWidget(self.channelLabel, 4, 3)
 
         self.resize(500, 500)
         self.move(100, 100)
@@ -48,14 +63,50 @@ class MakeDeviceImage(qt.QWidget):
                                       os.getcwd(),
                                       "Image files(*.jpg *png *.jpeg)")
         pixmap = gui.QPixmap(filename[0])
+        width = pixmap.width()
+        height = pixmap.height()
+
         self.imageCanvas.setPixmap(pixmap)
 
-    def setlabel(self):
-        """
-        Set the position for a channel label.
-        """
-        number = 10  # TODO: the user should input this
+        print(self.imageCanvas.frameSize())
 
+        # fix the image scale, so that the pixel values of the mouse are
+        # unambiguous
+        self.imageCanvas.setMaximumWidth(width)
+        self.imageCanvas.setMaximumHeight(height)
+
+    def setlabel_or_annotation(self, argument):
+        """
+        Set the position for a channel label or annotation
+        """
+
+        if argument not in ['label', 'annotation']:
+            raise ValueError('Only labels and annotations may be saved!')
+
+        number = self.channelLabel.getText()
+
+        self.imageCanvas.mousePressEvent = self._getpos
+        self.gotclick = False
+        while not self.gotclick:
+            self.qapp.processEvents()
+            time.sleep(0.01)  # Note: too high of a value makes clicking hard
+        self.imageCanvas.mousePressEvent = None
+
+        # update the data
+        if number not in self._data.keys():
+            self._data[number] = {}
+        self._data[number][argument] = (self.click_x, self.click_y)
+
+    def _getpos(self, event):
+        self.gotclick = True
+        self.click_x = event.pos().x()
+        self.click_y = event.pos().y()
+
+    def _drawitall(self):
+        """
+        Draws stuff... probably reusable.
+        """
+        pass
 
 class DeviceImage:
 
@@ -77,5 +128,5 @@ class DeviceImage:
 def testgui():
 
     app = qt.QApplication(sys.argv)
-    _ = MakeDeviceImage()
+    _ = MakeDeviceImage(app)
     sys.exit(app.exec_())
